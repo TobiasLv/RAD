77883716dab0b8f1f033564a40443b56c399108d
taoletian <taolt1999@outlook.com>
2024-06-13 16:53:36 +0800

    chore: Add setup.py for RAD package installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rad',
+    version='0.1.0',
+    author='iDLab',
+    author_email='lyo.tobias@foxmail.com',
+    description='A pytorch implementation of the optimizer RAD',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TobiasLv/RAD',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'torch',
+        'numpy',
+    ],
+)
